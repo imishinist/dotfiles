43f5249fa899c0d9cfe0ec88a54cd25f13d6e4bb
Taisuke Miyazaki <imishinist@gmail.com>
2016-02-12 15:37:29 +0900

    neobundleのインストールを追加

--- install.sh
+++ install.sh
@@ -2,9 +2,16 @@
 
 function install_prezto() {
     if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
-        invoke 'git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"'
+        git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
     fi
     return;
 }
 
+function install_neobundle() {
+    if [ ! -d "$HOME/.vim/bundle/neobundle.vim" ]; then
+        curl -s https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+    fi
+}
+
 install_prezto
+install_neobundle
