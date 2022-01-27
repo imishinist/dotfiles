d03f495e57be82d615e95eb5b11847decd94bf93
Taisuke Miyazaki <imishinist@gmail.com>
2022-01-27 13:59:51 +0900

    add submodule update

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 bin/setup
 
+if [ ! -f config/.config/nvim/dein/repos/github.com/Shougo/dein.vim ]; then
+    git submodule init && git submodule update --depth 1
+fi
+
 case "$(uname)" in
   "Darwin")  bin/mitamae local $@ lib/recipe.rb ;;
   *) sudo -E bin/mitamae local $@ lib/recipe.rb ;;
