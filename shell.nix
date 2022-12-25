a7bdb2d44987f9dd7df8c426812463adcfb25760
Taisuke Miyazaki <imishinist@gmail.com>
2022-12-25 19:57:58 +0900

    fix shell.nix for development

--- shell.nix
+++ shell.nix
@@ -1,9 +1,18 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+    in
+    import (fetchTree (fromJSON (readFile ./flake.lock)).nodes.nixpkgs.locked) {
+      overlays = [ ];
+    }
+  )
+}:
 
-with pkgs;
-mkShell{
+pkgs.mkShell
+{
   NIX_PATH = "nixpkgs=${builtins.toString pkgs.path}";
   nativeBuildInputs = [
     pkgs.nixpkgs-fmt
+    pkgs.home-manager
   ];
 }
