f7119405d250239f3160df891cdbae44e7870ef4
Taisuke Miyazaki <imishinist@gmail.com>
2023-12-13 22:43:12 +0900

    install starship from binary

--- cookbooks/starship/default.rb
+++ cookbooks/starship/default.rb
@@ -1,3 +1,11 @@
-brew 'starship'
+STARSHIP_VERSION = '1.16.0'.freeze
+
+cli_binary 'starship' do
+  repository 'starship/starship'
+  version "v#{STARSHIP_VERSION}"
+  platform = (node[:platform] == 'darwin' ? 'aarch64-apple-darwin' : 'x86_64-unknown-linux-musl')
+  release_name "starship-#{platform}.tar.gz"
+  target_dir "#{ENV['HOME']}/bin"
+end
 
 dotfile '.config/starship.toml'
