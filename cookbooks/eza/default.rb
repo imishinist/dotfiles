7ab4bf7e94fbe3ce909bdb4ea82fc684c27dfc1a
Taisuke Miyazaki <imishinist@gmail.com>
2023-12-13 23:23:35 +0900

    fix eza

--- cookbooks/eza/default.rb
+++ cookbooks/eza/default.rb
@@ -6,7 +6,7 @@ when "linux"
   cli_binary 'eza' do
     repository 'eza-community/eza'
     version "v#{EZA_VERSION}"
-    release_name "eza_x86_64_unknown-linux-musl.tar.gz"
+    release_name "eza_x86_64-unknown-linux-musl.tar.gz"
     target_dir "#{ENV['HOME']}/bin"
   end
 else
