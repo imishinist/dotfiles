c5199a3cc180be3c7ba7a3a59733e1c5ee2bcc60
Taisuke Miyazaki <imishinist@gmail.com>
2024-05-13 02:09:35 +0900

    fix typo

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 bin/setup
 
 echo "Checking submodule ..."
-SUBMODULE_NEED_UPDAE=0
+SUBMODULE_NEED_UPDATE=0
 for submodule in $(git submodule | awk '{print $2}'); do
     if [ ! -d "$submodule" ] || [ -z "$(ls -A $submodule)" ]; then
         echo "Need to fetch submodule \"$submodule\""
-        SUBMODULE_NEED_UPDAE=1
+        SUBMODULE_NEED_UPDATE=1
     fi
 done
 
-if [ $SUBMODULE_NEED_UPDAE -eq 1 ]; then
+if [ $SUBMODULE_NEED_UPDATE -eq 1 ]; then
     git submodule init && git submodule update --depth 1
 fi
 
