a876b53e58bc4fc44a4451ef7a373449749249b8
Taisuke Miyazaki <imishinist@gmail.com>
2025-04-07 12:43:13 +0900

    add direnv git-hook

--- config/.config/fish/conf.d/common.fish
+++ config/.config/fish/conf.d/common.fish
@@ -8,3 +8,17 @@ end
 
 bind \cs pet-select
 bind \er '__fzf_insert_reverse_isearch'
+
+function direnv-githook
+  set --local dir $(git rev-parse --show-toplevel)
+  set --local hook $dir/.git/hooks/post-checkout
+
+  if test -f $hook
+    echo "hook already exists: $hook"
+  else
+    echo "#!/bin/bash" > $hook
+    echo "direnv allow ." >> $hook
+    echo "hook created"
+  end
+  chmod +x $hook
+end
